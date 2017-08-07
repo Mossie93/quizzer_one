a5c61e93fc9271e440383e2ea77d92c0b1ef3520
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-08-07 18:06:37 +0200

    Apply changes to router which are required by the Coherence

--- web/router.ex
+++ web/router.ex
@@ -8,16 +8,42 @@ defmodule QuizzerOne.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session
+  end
+
+  pipeline :protected do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Coherence.Authentication.Session, protected: true
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :browser
+    coherence_routes()
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
+  end
+
+  # for public routes
   scope "/", QuizzerOne do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+  end
+
+  # for protected routes
+  scope "/", QuizzerOne do
+    pipe_through :protected
   end
 
   # Other scopes may use custom stacks.
