750e66de9785099748c9cb5e13c4c5ad5d6a3267
Rafał Mossakowski <rafal.mossakowski@netguru.pl>
2017-08-24 17:09:27 +0200

    Add plug for serving images

--- lib/quizzer_one/endpoint.ex
+++ lib/quizzer_one/endpoint.ex
@@ -11,6 +11,9 @@ defmodule QuizzerOne.Endpoint do
     at: "/", from: :quizzer_one, gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
+  plug Plug.Static,
+    at: "/", from: Path.expand("./uploads")
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
